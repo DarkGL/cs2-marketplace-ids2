ddb8df9f7c6f40d32046d4b3bd570817ac29bae2
Rafał Więcek <gitzzz14@gmail.com>
2024-11-14 19:10:15 +0100

    Update cron schedule and add market data handling

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,9 @@ import { request, fetch } from 'undici'
 
 import type { CSGOAPIResponse } from './types/CSGOAPIResponse.js';
 import type { CSMoneyIds } from './types/CSMoneyIds.js';
-import type { CSMoneyResponse, Item } from './types/CSMoneyResponse.js';
+import type { CSMoneyResponse, Item as ItemTrade } from './types/CSMoneyResponse.js';
+import type { CSMoneyMarketResponse } from './types/CSMoneyMarket.response.js';
+import type { Item as ItemMarket } from './types/CSMoneyMarket.response.js';
 
 const MAX_DOWNLOAD = 1500;
 
@@ -24,7 +26,7 @@ function writeCSMoneyIds(data: CSMoneyIds) {
     fs.writeFileSync('./csmoney-ids.json', JSON.stringify(data, null, 2));
 }
 
-async function loadFromCSMoney(name: string) {
+async function loadFromCSMoneyTrade(name: string) {
     return fetch(`https://cs.money/1.0/market/sell-orders?limit=60&offset=0&name=${name}`, {
         "headers": {
           "accept": "application/json, text/plain, */*",
@@ -47,8 +49,75 @@ async function loadFromCSMoney(name: string) {
       }).then((res) => res.json() as Promise<CSMoneyResponse>);
 }
 
-function findItem(name: string, items: Item[]) {
+async function loadFromCSMoneyMarket(name: string) {
+    return fetch(`https://cs.money/5.0/load_bots_inventory/730?limit=60&name=${name}&offset=0`, {
+        "headers": {
+          "accept": "application/json, text/plain, */*",
+          "accept-language": "en-US,en-GB;q=0.9,en;q=0.8,pl-PL;q=0.7,pl;q=0.6",
+          "cache-control": "no-cache",
+          "pragma": "no-cache",
+          "priority": "u=1, i",
+          "sec-ch-ua": "\"Chromium\";v=\"130\", \"Google Chrome\";v=\"130\", \"Not?A_Brand\";v=\"99\"",
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": "\"Windows\"",
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-origin",
+          "x-client-app": "web",
+          "Referer": "https://cs.money/csgo/trade/",
+          "Referrer-Policy": "strict-origin-when-cross-origin"
+        },
+        "body": null,
+        "method": "GET"
+      }).then((res) => res.json() as Promise<CSMoneyMarketResponse>);
+    }
+
+function findItemTrade(name: string, items: ItemTrade[]) {
     return items.find((item) => item.asset.names.full === name);
+}
+
+function findItemMarket(name: string, items: ItemMarket[]) {
+    return items.find((item) => item.fullName === name);
+}
+
+async function loadItemFromCSMoneyTrade(market_hash_name: string) {
+    const responseMoney = await loadFromCSMoneyTrade(market_hash_name);
+
+    if(!responseMoney || !responseMoney?.items || responseMoney?.items?.length === 0) {
+        console.log(`Item ${market_hash_name} has no CSMoney sell orders`);
+
+        return;
+    }
+
+    const itemMoney = findItemTrade(market_hash_name, responseMoney.items);
+
+    if(!itemMoney) {
+        console.log(`Item ${market_hash_name} has no CSMoney sell orders`);
+
+        return;
+    }
+
+    return itemMoney.asset.names.identifier;
+}
+
+async function loadItemFromCSMoneyMarket(market_hash_name: string) {
+    const responseMoney = await loadFromCSMoneyMarket(market_hash_name);
+
+    if(!responseMoney || !responseMoney?.items || responseMoney?.items?.length === 0) {
+        console.log(`Item ${market_hash_name} has no CSMoney sell orders`);
+
+        return;
+    }
+
+    const itemMoney = findItemMarket(market_hash_name, responseMoney.items);
+
+    if(!itemMoney) {
+        console.log(`Item ${market_hash_name} has no CSMoney sell orders`);
+
+        return;
+    }
+
+    return itemMoney.nameId;
 }
 
 async function main() {
@@ -101,27 +170,21 @@ async function main() {
             continue;
         }   
 
-        const responseMoney = await loadFromCSMoney(currentItem.market_hash_name);
+        let itemMoneyId = await loadItemFromCSMoneyTrade(currentItem.market_hash_name);
 
-        if(!responseMoney || !responseMoney?.items || responseMoney?.items?.length === 0) {
-            console.log(`Item ${item} has no CSMoney sell orders`);
+        if(!itemMoneyId) {
+            itemMoneyId = await loadItemFromCSMoneyMarket(currentItem.market_hash_name);
+        }
 
+        if(!itemMoneyId) {
             continue;
         }
 
-        const itemMoney = findItem(currentItem.market_hash_name, responseMoney.items);
-
-        if(!itemMoney) {
-            console.log(`Item ${item} ${currentItem.market_hash_name} has no CSMoney sell orders`);
-
-            continue;
-        }
-
-        console.log(`Item ${currentItem.market_hash_name} has CSMoney sell orders`, itemMoney.asset.names.identifier);
+        console.log(`Item ${currentItem.market_hash_name} has CSMoney sell orders`, itemMoneyId);
 
         csmoneyIds[currentItem.market_hash_name] = {
             name: currentItem.market_hash_name,
-            nameId: itemMoney.asset.names.identifier
+            nameId: itemMoneyId
         };
 
         writeCSMoneyIds(csmoneyIds);
